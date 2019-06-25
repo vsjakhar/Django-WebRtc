d60a792ee07686069ff7cd68942cf234b8e8ba44
Jyoti <vijayjakhar.vj@gmail.com>
2019-06-25 07:13:27 +0530

    Chat and Video both on same Page

---
+++ home/static/js/chat.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// var roomName = {{ room_name_json }};
+var roomName = 'st';
+
+var chatSocket = new WebSocket(
+	'ws://' + window.location.host +
+	'/ws/chat/' + roomName + '/');
+
+chatSocket.onmessage = function(e) {
+	var data = JSON.parse(e.data);
+	var message = data['message'];
+	document.querySelector('#chat-log').value += (message + '\n');
+};
+
+chatSocket.onclose = function(e) {
+	console.error('Chat socket closed unexpectedly');
+};
+
+document.querySelector('#chat-message-input').focus();
+document.querySelector('#chat-message-input').onkeyup = function(e) {
+	if (e.keyCode === 13) {  // enter, return
+		document.querySelector('#chat-message-submit').click();
+	}
+};
+
+document.querySelector('#chat-message-submit').onclick = function(e) {
+	var messageInputDom = document.querySelector('#chat-message-input');
+	var message = messageInputDom.value;
+	chatSocket.send(JSON.stringify({
+		'message': message
+	}));
+
+	messageInputDom.value = '';
+};
