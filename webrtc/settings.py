c176d0985fc6008f9f2b26152e7457dbaa4f2574
Jyoti <vijayjakhar.vj@gmail.com>
2019-07-02 15:23:26 +0530

    Redis Settings for Liveserver as dynamic

--- webrtc/settings.py
+++ webrtc/settings.py
@@ -131,7 +131,9 @@ CHANNEL_LAYERS = {
 	'default': {
 		'BACKEND': 'channels_redis.core.RedisChannelLayer',
 		'CONFIG': {
-			"hosts": [('127.0.0.1', 6379)],
+			# "hosts": [('127.0.0.1', 6379)],
+			# "hosts": [('localhost', 6379)],
+			"hosts": [os.environ.get('REDIS_URL','redis://localhost:6379')],
 		},
 	},
 }
