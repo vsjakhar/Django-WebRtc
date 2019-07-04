5b1b71cc7bbe01125fd994d08b2862f083e32858
Jyoti <vijayjakhar.vj@gmail.com>
2019-07-04 09:27:09 +0530

    Routing with django orl url function

--- chat/routing.py
+++ chat/routing.py
@@ -1,8 +1,9 @@
-# from django.conf.urls import url
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, re_path
 
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/', consumers.ChatConsumer),
+	url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+	# re_path('ws/chat/<str:room_name>/', consumers.ChatConsumer),
 ]
