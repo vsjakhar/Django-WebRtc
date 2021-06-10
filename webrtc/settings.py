72206cd5f6900bd13481f2d16bc9a2672ded72a3
Vijay Jakhar <vsjakhar07@gmail.com>
2021-06-10 10:18:24 +0530

    Upgrade to Django latest version

--- webrtc/settings.py
+++ webrtc/settings.py
@@ -125,6 +125,11 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Django Channels Setting
 ASGI_APPLICATION = 'webrtc.routing.application'
 CHANNEL_LAYERS = {
